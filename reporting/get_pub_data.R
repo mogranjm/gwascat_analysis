ffbeb574877976030e076a2dfed974b9c3cb9ade
James Morgan <ja.morgan1@outlook.com>
2019-07-25 02:42:25 +0000

    scrape each cancer publication abstract and doi from pubmed

---
+++ reporting/get_pub_data.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(xml2)
+library(readr)
+
+# Define project directories
+projroot <- rprojroot::find_rstudio_root_file()
+funcdir <- file.path(projroot, "functions")
+datadir <- file.path(projroot, "data")
+
+# Read cancer data
+gwas_cancer_data <- file.path(datadir, "gwas_cancer_classifications.csv") %>%
+    read_csv()
+
+# Get custom NCBI EUtil API functions
+api_key <- "d96bb10ffdca4cfb6ad3c963ba2e9f155d09"
+source(file.path(funcdir, "ncbi_eutil_functions.R"))
+
+# Extract PUBMEDIDs for each paper to query NCBI
+ids <- gwas_cancer_data %>%
+    distinct(PUBMEDID)
+
+id_str <- ids %>%
+    pull() %>%
+    paste(collapse = ",")
+
+# Construct query URL using NCBI EUtil API and query pubmed for publication data
+url <- eutil_fetch_url_generator("pubmed", id_str, rettype = "xml", api_key = api_key)
+
+xml <- read_xml(url)
+
+# Extract DOI and Abstract text from query result
+ids$doi <- xml_find_all(xml, '//ArticleId')[xml_attr(xml_find_all(xml, '//ArticleId'), "IdType")=="doi"] %>%
+    xml_text
+
+ids$abstract <- xml %>%
+    xml_find_all("//Abstract") %>%
+    xml_text() %>%
+    trimws()
+
+# Return extracted publication data to the original dataset and write to file.
+gwas_cancer_data <- gwas_cancer_data %>%
+    left_join(ids) %>%
+    write_csv(file.path(projroot, "data/gwas_cancer_classifications.csv"))
